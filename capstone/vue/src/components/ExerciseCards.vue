5bd5b06a661bbf28fb105a2e9d5baf7d0fe4a773
Raul Conde <r.conde2017@gmail.com>
2023-04-21 03:24:26 -0400

    Capstone 3 Updates 10

--- capstone/vue/src/components/ExerciseCards.vue
+++ capstone/vue/src/components/ExerciseCards.vue
@@ -1,33 +1,26 @@
 <template>
-  <div>
-    <div class="cards" v-for = "exercise in exercises" v-bind:key="exercise.exercise_id">
-      
-      <h2> {{exercise.exercise_name}} </h2>
-      <p> {{exercise.exercise_description}}</p>
-      <p>{{exercise.exercise_duration}}</p>
-      <p>{{exercise.target_area}}</p>
-      <p>{{exercise.suggested_weight}}</p>
-      <p>{{exercise.num_of_reps}}</p>
-     
-
-      
-
-    </div>
+  <div class="card-container">
+    <div class="cards" v-for="exercise in exercises" v-bind:key="exercise.exercise_id">
+      <h2>{{ exercise.exercise_name }}</h2>
+      <p>{{ exercise.exercise_description }}</p>
+      <div class="exercise-details">
+        <p><strong>Duration:</strong> {{ exercise.exercise_duration }}</p>
+        <p><strong>Target Area:</strong> {{ exercise.target_area }}</p>
+        <p><strong>Suggested Weight:</strong> {{ exercise.suggested_weight }}</p>
+        <p><strong>Number of Reps:</strong> {{ exercise.num_of_reps }}</p>
       </div>
-  
+    </div>
+  </div>
 </template>
 
 <script>
-
-import ExerciseService from '../services/ExerciseService.js'
+import ExerciseService from "../services/ExerciseService.js";
 
 export default {
-created() {
-    ExerciseService.listExercises().then (
-      response=> {
-        this.exercises=response.data
-      }
-    )
+  created() {
+    ExerciseService.listExercises().then((response) => {
+      this.exercises = response.data;
+    });
   },
 
   data() {
@@ -39,29 +32,40 @@ created() {
         exercise_duration: 0,
         target_area: "",
         suggested_weight: 0,
-        num_of_reps: 0
+        num_of_reps: 0,
       },
       exercises: [],
-      filters:[],
-
+      filters: [],
     };
   },
-  methods: {
-    
-  }
-
-}
+  methods: {},
+};
 </script>
 
 <style>
-
-.cards {
-    display: inline-block;
-    width: 25%;
-    border: solid 3px white;
-    padding: 10px;
-    margin: 5px;
-    border-radius: 15px;;
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
+.cards {
+  display: flex;
+  flex-direction: column;
+  width: calc(33.33% - 10px);
+  margin: 5px;
+  border-radius: 15px;
+  background-color: #f7f7f7;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  padding: 10px;
+}
+
+h2 {
+  font-size: 1.5rem;
+  margin: 0;
+}
+
+.exercise-details {
+  margin-top: auto;
+}
 </style>
